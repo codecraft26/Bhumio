9a11433073521531ee08bd200ed94879fab9e6da
codecraft26 <gaman0221@gmail.com>
2023-02-28 02:18:31 +0530

    adding event emitter in auth module for sending mail after user is created by admin

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,15 @@ import { ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PassportModule } from '@nestjs/passport';
+import { ScheduleModule } from '@nestjs/schedule';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
 
 import { UserSchema } from './schema/user.schema';
+import { NodemailerService } from 'src/nodemailer/nodemailer.service';
+import { NodemailerModule } from 'src/nodemailer/nodemailer.module';
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
@@ -23,7 +27,12 @@ import { UserSchema } from './schema/user.schema';
       },
     }),
     MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
-  ],
+
+    NodemailerModule
+  
+  
+
+],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
   exports: [JwtStrategy, PassportModule,AuthService],
